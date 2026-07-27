c1c98d21107ed3ab2bd24069583d58fb86356a0d
Noobtoss <thomasschmittba17196@gmail.com>
2026-07-27 14:56:53 +0200

    updated runtime.py

--- custom/configs/runtime.py
+++ custom/configs/runtime.py
@@ -10,8 +10,8 @@ vis_backends = [
     dict(
         type='WandbVisBackend',
         init_kwargs=dict(
-            project='runs',
-            name='tmp-premium'
+            project='runs-mmdetection',
+            name='unnamed_run'
         )
     )
 ]
@@ -29,7 +29,7 @@ default_hooks = dict(
     timer=dict(type='IterTimerHook'),
     logger=dict(type='LoggerHook', interval=50),
     param_scheduler=dict(type='ParamSchedulerHook'),
-    checkpoint=dict(type='CheckpointHook', interval=1),
+    checkpoint=dict(type='CheckpointHook', interval=1, max_keep_ckpts=1),  # dict(type='CheckpointHook', interval=1),
     sampler_seed=dict(type='DistSamplerSeedHook'),
     visualization=dict(type='DetVisualizationHook'))
 
