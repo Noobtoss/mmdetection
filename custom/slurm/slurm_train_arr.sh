66d19e82a93726e91b2fef01ae3d1b98848a6429
Noobtoss <thomasschmittba17196@gmail.com>
2026-07-27 17:56:59 +0200

    refactoring

--- custom/slurm/slurm_train_arr.sh
+++ custom/slurm/slurm_train_arr.sh
@@ -10,8 +10,9 @@
 #SBATCH --cpus-per-task=1          # CPU Kerne pro Task (>1 für multi-threaded Tasks)
 #SBATCH --mem-per-cpu=64G          # RAM pro CPU Kern #20G #32G #64G
 
-# ----- ROOT_DIR ----------------------------------------------------
+# ----- DIRS --------------------------------------------------------
 ROOT_DIR=/nfs/scratch/staff/schmittth/code_nexus/mmdetection
+export TMPDIR=$(mktemp -d "${TMPDIR:-/tmp}/mmdetection_${SLURM_JOB_ID}_XXXXXX")
 
 # ----- GET ARGS ----------------------------------------------------
 PARAMS_FILE="$ROOT_DIR/custom/slurm/slurm_params.txt"
@@ -46,7 +47,6 @@ eval "$(conda shell.bash hook)"
 conda activate conda-mmdetection
 
 export PYTHONPATH="$ROOT_DIR/custom/src:$PYTHONPATH"
-export TMPDIR=$(mktemp -d "${TMPDIR:-/tmp}/mmdetection_${SLURM_JOB_ID}_XXXXXX")
 
 # ----- WANDB -------------------------------------------------------
 export WANDB_API_KEY=95177947f5f36556806da90ea7a0bf93ed857d58
