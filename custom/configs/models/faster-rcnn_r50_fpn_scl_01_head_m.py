32c121930cfa4b694303bc8e358aeda3c24951de
Noobtoss <thomasschmittba17196@gmail.com>
2026-08-03 23:04:04 +0200

    updating configs

--- custom/configs/models/faster-rcnn_r50_fpn_scl_01_head_m.py
+++ custom/configs/models/faster-rcnn_r50_fpn_scl_01_head_m.py
@@ -4,7 +4,7 @@ model = dict(
     roi_head=dict(
         bbox_head=dict(
             cls_feat_loss = dict(
-                type='ClsFeatLoss', loss='sup_con_loss', loss_weight=0.2  # loss_weight=0
+                type='ClsFeatLoss', loss='sup_con_loss', loss_weight=0.1  # loss_weight=0
             ),
             cls_feat_proj_head = dict(
                 type='ClsFeatProjHead', proj_head='m', dim=1024,  # hard encoding bad
