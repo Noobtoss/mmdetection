160bc56ec7bfc537eb5f306976f980e58635a1c6
Noobtoss <thomasschmittba17196@gmail.com>
2026-08-03 21:44:45 +0200

    updating configs

--- custom/configs/models/faster-rcnn_r50_fpn_scl_02.py
+++ custom/configs/models/faster-rcnn_r50_fpn_scl_02.py
@@ -4,7 +4,7 @@ model = dict(
     roi_head=dict(
         bbox_head=dict(
             cls_feat_loss = dict(
-                type='ClsFeatLoss', loss='sup_con_loss', loss_weight=1  # loss_weight=0
+                type='ClsFeatLoss', loss='sup_con_loss', loss_weight=0.2  # loss_weight=0
             )
         )
     )
