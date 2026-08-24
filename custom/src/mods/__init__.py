48fe4d9276afe1d0523f634dcf111c655aebb04e
Noobtoss <thomasschmittba17196@gmail.com>
2026-08-24 16:10:24 +0200

    refactoring

--- custom/src/mods/__init__.py
+++ custom/src/mods/__init__.py
@@ -1,5 +1,9 @@
+# mods model eval
 from .coco_metric import CocoMetric
 from .wandb_vis_backend import WandbVisBackend
+# mods model architectures
 from .standard_roi_head import StandardRoIHead
-from .convfc_bbox_head import Shared2FCBBoxHead
+from .shared2fc_bbox_head import Shared2FCBBoxHead
+from .detr_head import DETRHead
+# mods training loss
 from .mmdet_register_module import ClsFeatLoss, ClsFeatProjHead
