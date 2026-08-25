f0e10823fd3723f1bd0c76f043e8cd86bddf7f89
Noobtoss <thomasschmittba17196@gmail.com>
2026-08-25 15:31:23 +0200

    fixed

--- custom/slurm/alex/slurm_train.sh
+++ custom/slurm/alex/slurm_train.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=mmdetection_train # Kurzname des Jobs
-#SBATCH --array=1-5%5
+#SBATCH --array=6-8%5
 #SBATCH --output=logs/R_%A_%a.out
 #SBATCH --gres=gpu:a40:1     # Request 1x A40 GPUs
 #SBATCH --partition=a40      # Submit to the a40 node partition
