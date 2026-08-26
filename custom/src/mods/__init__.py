38c3ccc60cfcc595bd8506b5b8ac9806c0f98364
Noobtoss <thomasschmittba17196@gmail.com>
2026-08-26 15:24:21 +0200

    init experiments initalization

--- custom/src/mods/__init__.py
+++ custom/src/mods/__init__.py
@@ -5,5 +5,7 @@ from .wandb_vis_backend import WandbVisBackend
 from .standard_roi_head import StandardRoIHead
 from .shared2fc_bbox_head import Shared2FCBBoxHead
 from .detr_head import DETRHead
+from .deformable_detr_head import DeformableDETRHead
+from .dino_head import DINOHead
 # mods training loss
 from .mmdet_register_module import ClsFeatLoss, ClsFeatProjHead
