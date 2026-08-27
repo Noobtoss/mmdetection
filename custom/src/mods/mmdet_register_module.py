f17250acd29b1dc811bffa1c9783937304cfb791
Noobtoss <thomasschmittba17196@gmail.com>
2026-08-27 15:34:04 +0200

    added rolling average to logging_loss

--- custom/src/mods/mmdet_register_module.py
+++ custom/src/mods/mmdet_register_module.py
@@ -1,3 +1,4 @@
+from collections import deque
 import torch
 from mmdet.registry import MODELS
 
@@ -7,14 +8,18 @@ from .cls_feat_proj_head import ClsFeatProjHead
 
 @MODELS.register_module()
 class ClsFeatLoss(_ClsFeatLoss):
-    def __init__(self, *args, loss_weight: float = 1, **kwargs) -> None:
+    def __init__(self, *args, loss_weight: float = 1, log_window_size: int = 10, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self.loss_weight = loss_weight
         self.logging_cache = None
+        self.log_window_size = log_window_size
+        self._loss_history = deque(maxlen=log_window_size)
 
     def forward(self, *args, **kwargs) -> torch.Tensor:
         loss = super().forward(*args, **kwargs)
-        self.logging_cache = loss.detach()
+        detached = loss.detach()
+        self._loss_history.append(detached)
+        self.logging_cache = sum(self._loss_history) / len(self._loss_history)
         return loss * self.loss_weight
 
     def get_logging(self) -> torch.Tensor:
